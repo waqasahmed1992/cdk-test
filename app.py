de02592df45acaed2adaf9d4879ab8665a0f7e7f
waqas <waq.ahmed01@gmail.com>
2022-12-22 17:51:13 +0500

    pushing changes

--- app.py
+++ app.py
@@ -3,26 +3,41 @@ import os
 
 import aws_cdk as cdk
 
-from test.test_stack import TestStack
+from test.test_stack import ArtifactBucketClass
+
+
 
 
 app = cdk.App()
-TestStack(app, "TestStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
+env_dev=cdk.Environment(region=app.node.try_get_context('dev')['region'])
+env_prod=cdk.Environment(region=app.node.try_get_context('prod')['region'])
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+# print(app.node.try_get_context{"prod"}["region"])
 
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
+# app.node.try_get_context{"prod"}[region]
 
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
+# print('@aws-cdk/aws-events:eventsTargetQueueSameAccount')
 
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+ArtifactBucketClass(app, "MyDevStack",env=env_dev)
+ArtifactBucketClass(app, "MyProdStack",is_prod=True,env=env_prod)
+
+# ArtifactBucketClass(app, "TestStack",
+#     # If you don't specify 'env', this stack will be environment-agnostic.
+#     # Account/Region-dependent features and context lookups will not work,
+#     # but a single synthesized template can be deployed anywhere.
+
+#     # Uncomment the next line to specialize this stack for the AWS Account
+#     # and Region that are implied by the current CLI configuration.
+
+#     #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+
+#     # Uncomment the next line if you know exactly what Account and Region you
+#     # want to deploy the stack to. */
+
+#     #env=cdk.Environment(account='123456789012', region='us-east-1'),
+
+#     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+#     )
 
 app.synth()
--- test/test_stack.py
+++ test/test_stack.py
@@ -6,14 +6,19 @@ from aws_cdk import (
 )
 from constructs import Construct
 
-class TestStack(Stack):
+class ArtifactBucketClass(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, is_prod=False, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        mybucket = s3.Bucket(self, "mybucket1",
-                             versioned=False,
-                             block_public_access=s3.BlockPublicAccess.BLOCK_ALL)
+        if is_prod:
+            mybucket = s3.Bucket(self, "mybucket1",
+                                 versioned=self.node.try_get_context('prod')['versioned'],
+                                 block_public_access=s3.BlockPublicAccess.BLOCK_ALL)
+        else:
+            mybucket = s3.Bucket(self,"nybucket1",
+                                 versioned=self.node.try_get_context('dev')['versioned'],
+                                 public_read_access=True)
         
         # The code that defines your stack goes here
 
